f85ac85bfe32369caf2735bea526e43252571ab6
Vinícius <viniciuslopes.dev@gmail.com>
2016-06-19 22:47:10 -0300

    Code language in English

--- main.c
+++ main.c
@@ -1,77 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX_VECTOR_INDICE 7
+#define MAX_INDICE 7
 
- int i,z,aux,num_trocas=0;
-
+int i,z,aux,num_chages=0;
 
 int main(int argc, char *argv[])
 {
-  int vetor[7] ={1,4,2,6,5,4,3};
+  int vector[7] ={1,4,2,6,5,4,3};
   printf("*******/ SORTER OF NUMBERS \\********\n\n");
-  printf("Vetor inicial: ");
-  for(i=0;i<MAX_VECTOR_INDICE; i++){
-       printf("%d",vetor[i]);
-       if(i < (MAX_VECTOR_INDICE-1)){
+  printf("Initial vector: ");
+  for(i=0;i<MAX_INDICE; i++){
+       printf("%d",vector[i]);
+       if(i < (MAX_INDICE-1)){
            printf(", ");
        }
   }
   printf("\n");
-  bubbleSortMethod(&vetor);
-  SelectionSortMethod(&vetor);
-
-
+  bubbleSortMethod(&vector);
+  SelectionSortMethod(&vector);
 
   system("PAUSE");
   return 0;
 }
 
-
-void bubbleSortMethod(int * vetor){
+void bubbleSortMethod(int * vector){
     char method_name[25] = "Bubble Sort";
-    for(i=MAX_VECTOR_INDICE-1;i>=0; i--){
+    for(i=MAX_INDICE-1;i>=0; i--){
         for(z=0;z<i; z++){
-            if(vetor[z+1]<vetor[z]){
-            aux =  vetor[z];
-            vetor[z]=vetor[z+1];
-            vetor[z+1]=aux;
-            num_trocas++;
+            if(vector[z+1]<vector[z]){
+            aux =  vector[z];
+            vector[z]=vector[z+1];
+            vector[z+1]=aux;
+            num_chages++;
            }
          }
     }
-      imprimeVetorOrdenado(vetor, num_trocas, &method_name);
+      printVector(vector, num_chages, &method_name);
 }
 
 void QuickSortMethod(){
 //TODO
 }
 
-void SelectionSortMethod(int * vetor){
+void SelectionSortMethod(int * vector){
      char method_name[25] = "Selection Sort";
-     int menor;
-     for(i=0;i<MAX_VECTOR_INDICE; i++){
-        menor=i;
-        for(z=i;z<MAX_VECTOR_INDICE; z++){
-            if(vetor[z]<vetor[i]){
-                menor = z;
+     int less;
+     for(i=0;i<MAX_INDICE; i++){
+        less=i;
+        for(z=i;z<MAX_INDICE; z++){
+            if(vector[z]<vector[i]){
+                less = z;
             }
         }
-        aux =  vetor[i];
-        vetor[i]=vetor[menor];
-        vetor[menor]=aux;
-        num_trocas++;
+        aux =  vector[i];
+        vector[i]=vector[less];
+        vector[less]=aux;
+        num_chages++;
     }
-    imprimeVetorOrdenado(vetor, num_trocas, &method_name);
+    printVector(vector, num_chages, &method_name);
 }
 
 
-void imprimeVetorOrdenado(int * vetor, int num_trocas, char * method_name){
-     printf("\n\nVetor ordenado pelo metodo: %s \n",method_name);
-     printf("Numero de trocas: %d \n",num_trocas);
-     printf("Vetor ordenado: ");
-     for(i=0;i<=MAX_VECTOR_INDICE-1; i++){
-         printf("%d, ",vetor[i]);
-
+void printVector(int * vector, int num_chages, char * method_name){
+     printf("\n\nVector ordered by the method: %s \n",method_name);
+     printf("Number of exchanges: %d \n",num_chages);
+     printf("Sorted vector: ");
+     for(i=0;i<=MAX_INDICE-1; i++){
+         printf("%d, ",vector[i]);
      }
      printf("\n");
 }
